7d2f41801a6a3660aa0624c341a7cc20da692734
JHRA-Csp <jack.allan@ucas-edu.net>
2026-09-04 14:30:11 -0600

    First Commit

---
+++ test.py
@@ -0,0 +1,3 @@
+# JA, 7th, First program
+
+print("Hello World!")                                                                                                                                                                                                                                           
