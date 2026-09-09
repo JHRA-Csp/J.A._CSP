a197d67ec8d6b9add793adcd78dae6ab1ee72518
JHRA-Csp <jack.allan@ucas-edu.net>
2026-09-09 14:52:25 -0600

    Variables Notes

---
+++ Variables_Notes.py
@@ -0,0 +1,7 @@
+# JA, Variables Notes <= for people to read, not for the computer.
+
+name = input("Tell me your name: ")
+
+print("CSP is the best class never!!!")
+print("Pink classrooms are the best!")
+print("Hello" ,name, "Welcome to class!")
