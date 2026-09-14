f8020c7f74743e75a53e67a5a57303b383fdd5ed
JHRA-Csp <jack.allan@ucas-edu.net>
2026-09-14 14:48:13 -0600

    memory diagram Activity

---
+++ Logical Memory.py
@@ -0,0 +1,9 @@
+# JA, Logical and Physical memory notes
+
+pi = 3.141592
+
+school = "UCAS"
+
+print(f"The Variable pi has a value of {pi} and is saved at {id(pi)}")
+
+print(f"the variable school has a value of {school} and is saved at {id(school)}")
---
+++ Memory-Diagram-Activity-HW.py
@@ -0,0 +1,28 @@
+# JA, Memory Diagram Activity
+
+Name = "Alex"
+print(f"The variable Alex has a value of {"Alex"} and is saved at {id(Name)}")
+
+Age = "14"
+print(f"The variable 14 has a value of {"14"} and is saved at {id(Age)}")
+
+Score = "10"
+print(f"The variable 10 has a value of {"10"} and is saved at {id(Score)}")
+
+Score = "25"
+print(f"The variable 25 has a value of {"25"} and is saved at {id(Score)}")
+
+Height = "5.9"
+print(f"The variable 5.9 has a value of {"5.9"} and is saved at {id(Height)}")
+
+City = "Denver"
+print(f"The variable Denver has a value of {"Denver"} and is saved at {id(City)}")
+
+Zip_Code = "80202"
+print(f"The variable 80202 has a value of {"80202"} and is saved at {id(Zip_Code)}")
+
+Price1 = "10"
+print(f"The variable 10 has a value of {"10"} and is saved at {id(Price1)}")
+
+Price2 = "10"
+print(f"The variable 10 has a value of {"10"} and is saved at {id(Price2)}")
