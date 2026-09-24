2452294de9fa9f02077a4101df2c960a2d4061e0
JHRA-Csp <jack.allan@ucas-edu.net>
2026-09-24 14:15:21 -0600

    Password

--- Password_strength_Checker.py
+++ Password_strength_Checker.py
@@ -22,3 +22,24 @@ if Password.islower():
 if Password.isupper():
     print("Your password has enough capitals")
     Uppercase = True
+
+if Password.isupper():
+    print("You should add more lowercase characters for an even stronger password")
+    print("FAILED, resart program to try again")
+if Password.islower():
+    print("Your password has enough lowercase letters")
+    Lowercase = True
+
+if Password.isnumeric():
+    print("You should add some numbers for an even stronger password")
+    print("FAILED, resart program to try again")
+if Password.isnumeric():
+    print("Your password has enough numbers")
+    Lowercase = True
+
+if letter != Password:
+    print("You should add some numbers for an even stronger password like !@#$%^&*?: ")
+    print("FAILED, resart program to try again")
+if letter in Password:
+    print("Your password has enough symbols")
+    letter = True
